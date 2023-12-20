48a8760a59492edea7dafbaf8746feaa46571b85
Dasha Chernova <pro100turistka@yandex.ru>
2023-12-20 16:08:44 +0700

    Добавьте файлы проекта.

---
+++ WindowsFormsApp5/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace WindowsFormsApp5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.openGLControl1 = new SharpGL.OpenGLControl();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.trackBar4 = new System.Windows.Forms.TrackBar();
+            this.trackBar5 = new System.Windows.Forms.TrackBar();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.openGLControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openGLControl1
+            // 
+            this.openGLControl1.DrawFPS = false;
+            this.openGLControl1.Location = new System.Drawing.Point(13, 13);
+            this.openGLControl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.openGLControl1.Name = "openGLControl1";
+            this.openGLControl1.OpenGLVersion = SharpGL.Version.OpenGLVersion.OpenGL2_1;
+            this.openGLControl1.RenderContextType = SharpGL.RenderContextType.DIBSection;
+            this.openGLControl1.RenderTrigger = SharpGL.RenderTrigger.TimerBased;
+            this.openGLControl1.Size = new System.Drawing.Size(1000, 559);
+            this.openGLControl1.TabIndex = 0;
+            this.openGLControl1.OpenGLInitialized += new System.EventHandler(this.openGLControl1_OpenGLInitialized);
+            this.openGLControl1.OpenGLDraw += new SharpGL.RenderEventHandler(this.openGLControl1_OpenGLDraw_1);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(1020, 92);
+            this.trackBar1.Maximum = 1000;
+            this.trackBar1.Minimum = -1000;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar1.Size = new System.Drawing.Size(56, 480);
+            this.trackBar1.TabIndex = 1;
+            this.trackBar1.TickFrequency = 10;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(1082, 92);
+            this.trackBar2.Maximum = 1000;
+            this.trackBar2.Minimum = -1000;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar2.Size = new System.Drawing.Size(56, 480);
+            this.trackBar2.TabIndex = 2;
+            this.trackBar2.TickFrequency = 10;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.Location = new System.Drawing.Point(1155, 92);
+            this.trackBar3.Minimum = -10;
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar3.Size = new System.Drawing.Size(56, 480);
+            this.trackBar3.TabIndex = 3;
+            this.trackBar3.Scroll += new System.EventHandler(this.trackBar3_Scroll);
+            // 
+            // trackBar4
+            // 
+            this.trackBar4.Location = new System.Drawing.Point(1217, 92);
+            this.trackBar4.Maximum = 360;
+            this.trackBar4.Name = "trackBar4";
+            this.trackBar4.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar4.Size = new System.Drawing.Size(56, 480);
+            this.trackBar4.TabIndex = 4;
+            this.trackBar4.Scroll += new System.EventHandler(this.trackBar4_Scroll);
+            // 
+            // trackBar5
+            // 
+            this.trackBar5.Location = new System.Drawing.Point(1279, 92);
+            this.trackBar5.Maximum = 1000;
+            this.trackBar5.Name = "trackBar5";
+            this.trackBar5.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar5.Size = new System.Drawing.Size(56, 480);
+            this.trackBar5.TabIndex = 5;
+            this.trackBar5.Scroll += new System.EventHandler(this.trackBar5_Scroll);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Вращение вдоль оси Х",
+            "Вращение вдоль оси Y",
+            "Вращение вдоль оси Z"});
+            this.comboBox1.Location = new System.Drawing.Point(1031, 23);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(294, 24);
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(1027, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "X";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(1089, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Y";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(1144, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(18, 20);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Z";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(1213, 69);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 20);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Angle";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(1279, 69);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 20);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Zoom";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1337, 584);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.trackBar5);
+            this.Controls.Add(this.trackBar4);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.openGLControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.openGLControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private SharpGL.OpenGLControl openGLControl1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.TrackBar trackBar4;
+        private System.Windows.Forms.TrackBar trackBar5;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ WindowsFormsApp5/Form1.cs
@@ -0,0 +1,327 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using SharpGL;
+
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+       
+        public float x;
+        public float y;
+        int os_x = 1, os_y = 0, os_z = 0, count = 0; // выбранные оси 
+        double a = 0, b = 0, c = -6, d = 0, zoom = 1, flag=0;
+        myReadObj obj = new myReadObj();
+        
+     
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+        }
+
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            b = (double)trackBar2.Value/1000 ;
+        }
+
+        private void trackBar3_Scroll(object sender, EventArgs e)
+        {
+            c = (double)trackBar3.Value / 1000;
+        }
+
+        private void trackBar4_Scroll(object sender, EventArgs e)
+        {
+            d = (double)trackBar4.Value;
+        }
+
+        private void trackBar5_Scroll(object sender, EventArgs e)
+        {
+            zoom = (double)trackBar5.Value/1000;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    {
+                        os_x = 1; os_y = 0; os_z = 0;
+                        break;
+                    }
+                case 1:
+                    {
+                        os_x = 0; os_y = 1; os_z = 0;
+                        break;
+                    }
+                case 2:
+                    {
+                        os_x = 0; os_y = 0; os_z = 1;
+                        break;
+                    }
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        class myReadObj
+        {
+            
+            public class POINT3
+            {
+                public double X;
+                public double Y;
+                public double Z;
+            };
+            public class WenLi
+            {
+                public double TU;
+                public double TV;
+            };
+            public class FaXiangLiang
+            {
+                public double NX;
+                public double NY;
+                public double NZ;
+            };
+            public class Mian
+            {
+                public int[] V = new int[3];
+                public int[] T = new int[3];
+                public int[] N = new int[3];
+            };
+            public class Model
+            {
+
+                public List<POINT3> V = new List<POINT3>();
+                public List<WenLi> VT = new List<WenLi>();
+                public List<FaXiangLiang> VN = new List<FaXiangLiang>();
+                public List<Mian> F = new List<Mian>();
+                                                       
+            }
+
+            public Model mesh = new Model();
+            
+            
+            public static float scale;
+           
+            public uint showFaceList;
+
+            public int YU = 1;
+
+            public void loadFile(String fileName)
+            {
+                // Mian[] f;
+                //POINT3[] v;
+                //FaXiangLiang[] vn;
+                //WenLi[] vt;
+
+                StreamReader objReader = new StreamReader(fileName);
+                ArrayList al = new ArrayList();
+                string texLineTem = "";
+                while (objReader.Peek() != -1)
+                {
+                    texLineTem = objReader.ReadLine();
+                    if (texLineTem.Length < 2) continue;
+                    if (texLineTem.IndexOf("v") == 0)
+                    {
+                        if (texLineTem.IndexOf("t") == 1)//Vt
+                        {
+                            string[] tempArray = texLineTem.Split(' ');
+                            WenLi vt = new WenLi();
+                            tempArray[1] = tempArray[1].Replace(".", ",");
+                            tempArray[2] = tempArray[2].Replace(".", ",");
+                            vt.TU = double.Parse(tempArray[1]);
+                            vt.TV = double.Parse(tempArray[2]);
+                            mesh.VT.Add(vt);
+                        }
+                        else if (texLineTem.IndexOf("n") == 1)//VN 
+                        {
+                            string[] tempArray = texLineTem.Split(new char[] { '/', ' ' }, System.StringSplitOptions.RemoveEmptyEntries);
+                            FaXiangLiang vn = new FaXiangLiang();
+                            tempArray[1] = tempArray[1].Replace(".", ",");
+                            tempArray[2] = tempArray[2].Replace(".", ",");
+                            tempArray[3] = tempArray[3].Replace(".", ",");
+                            vn.NX = double.Parse(tempArray[1]);
+                            vn.NY = double.Parse(tempArray[2]);
+
+                            if (tempArray[3] == "\\")
+                            {
+                                texLineTem = objReader.ReadLine();
+                                vn.NZ = double.Parse(texLineTem);
+                            }
+                            else vn.NZ = double.Parse(tempArray[3]);
+
+                            mesh.VN.Add(vn);
+                        }
+                        else
+                        {//V
+                            string[] tempArray = texLineTem.Split(' ');
+                            POINT3 v = new POINT3();
+                            System.Console.WriteLine(tempArray[1]);
+                            tempArray[1] = tempArray[1].Replace(".", ",");
+                            tempArray[2] = tempArray[2].Replace(".", ",");
+                            tempArray[3] = tempArray[3].Replace(".", ",");
+
+                            if (string.IsNullOrEmpty(tempArray[1]) == true)
+                            {
+                                tempArray[4] = tempArray[4].Replace(".", ",");
+                                v.X = double.Parse(tempArray[2]);
+                                v.Y = double.Parse(tempArray[3]);
+                                v.Z = double.Parse(tempArray[4]);
+                            }
+                            else
+                            {
+                                v.X = double.Parse(tempArray[1]);
+                                v.Y = double.Parse(tempArray[2]);
+                                v.Z = double.Parse(tempArray[3]);
+                            }
+                            
+                            mesh.V.Add(v);
+                        }
+                    }
+                    else if (texLineTem.IndexOf("f") == 0)
+                    {
+                        //f 
+                        string[] tempArray = texLineTem.Split(new char[] { '/', ' ' }, System.StringSplitOptions.RemoveEmptyEntries);
+                        Mian f = new Mian();
+                        int i = 0;
+                        int k = 1;
+                        while (i < 3)
+                        {
+                            if (mesh.V.Count() != 0)
+                            {
+                                f.V[i] = int.Parse(tempArray[k]) - 1;
+                                k++;
+                            }
+                            if (mesh.VT.Count() != 0)
+                            {
+                                f.T[i] = int.Parse(tempArray[k]) - 1;
+                                k++;
+                            }
+                            if (mesh.VN.Count() != 0)
+                            {
+                                f.N[i] = int.Parse(tempArray[k]) - 1;
+                                k++;
+                            }
+                            i++;
+                        }
+                        mesh.F.Add(f);
+
+                    }
+                }
+
+
+            }
+
+            public uint createListFace(ref SharpGL.OpenGL gl)
+            {
+                int i = 0;
+                 gl.NewList(showFaceList, OpenGL.GL_COMPILE);
+                for (i = 0; i < mesh.F.Count(); i++)
+                    
+                {
+                 
+                    gl.Begin(OpenGL.GL_TRIANGLES);                          //  Рисуем полигоны 
+                    if (mesh.VT.Count() != 0) gl.TexCoord(mesh.VT[mesh.F[i].T[0]].TU, mesh.VT[mesh.F[i].T[0]].TV);  //текстура
+                    if (mesh.VN.Count() != 0) gl.Normal(mesh.VN[mesh.F[i].N[0]].NX, mesh.VN[mesh.F[i].N[0]].NY, mesh.VN[mesh.F[i].N[0]].NZ);
+                    gl.Vertex(mesh.V[mesh.F[i].V[0]].X / YU, mesh.V[mesh.F[i].V[0]].Y / YU, mesh.V[mesh.F[i].V[0]].Z / YU);        //  вверх
+                    if (mesh.VT.Count() != 0) gl.TexCoord(mesh.VT[mesh.F[i].T[1]].TU, mesh.VT[mesh.F[i].T[1]].TV);  //текстура
+                    if (mesh.VN.Count() != 0)gl.Normal(mesh.VN[mesh.F[i].N[1]].NX, mesh.VN[mesh.F[i].N[1]].NY, mesh.VN[mesh.F[i].N[1]].NZ);
+                    gl.Vertex(mesh.V[mesh.F[i].V[1]].X / YU, mesh.V[mesh.F[i].V[1]].Y / YU, mesh.V[mesh.F[i].V[1]].Z / YU);        //  нижний левый
+                    if (mesh.VT.Count() != 0) gl.TexCoord(mesh.VT[mesh.F[i].T[2]].TU, mesh.VT[mesh.F[i].T[2]].TV);  //текстура
+                    if (mesh.VN.Count() != 0)gl.Normal(mesh.VN[mesh.F[i].N[2]].NX, mesh.VN[mesh.F[i].N[2]].NY, mesh.VN[mesh.F[i].N[2]].NZ);
+                    gl.Vertex(mesh.V[mesh.F[i].V[2]].X / YU, mesh.V[mesh.F[i].V[2]].Y / YU, mesh.V[mesh.F[i].V[2]].Z / YU);        //  нижний правый
+
+
+
+                    gl.End();
+                    
+
+                   
+                }
+                gl.EndList();
+                return showFaceList;
+
+            }
+            
+
+
+        }
+        private void openGLControl1_OpenGLDraw(object sender, SharpGL.RenderEventArgs args)
+        {
+            
+
+        }
+
+        float[] glfMatAmbient = { 0.200f, 0.200f, 0.200f, 1.0f };
+        float[] glfMatDiffuse = { 0.200f, 0.200f, 0.200f, 1.0f };
+        float[] glfMatSpecular = { 1.000f, 1.000f, 1.000f, 1.0f };
+        float[] glfMatEmission = { 0.000f, 0.000f, 0.000f, 1.0f };
+        float fShininess = 50.000f;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+          
+        }
+
+        private void openGLControl1_OpenGLDraw_1(object sender, SharpGL.RenderEventArgs args)
+        {
+
+            SharpGL.OpenGL gl = this.openGLControl1.OpenGL;
+            gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
+            gl.LoadIdentity();
+
+            float[] glfLight = { -4.0f, 4.0f, 4.0f, 0.0f };
+            gl.Light(OpenGL.GL_LIGHT0, OpenGL.GL_POSITION, glfLight);
+
+           
+            gl.Translate(a, b, c);
+
+            // поворот
+            gl.Rotate(d, os_x, os_y, os_z);
+            // масштабирование
+            gl.Scale(zoom, zoom, zoom);
+    
+            obj.createListFace(ref gl);
+            gl.CallList(obj.showFaceList);
+
+           
+        }
+
+        private void openGLControl1_OpenGLInitialized(object sender, EventArgs e)
+        {
+            SharpGL.OpenGL gl = this.openGLControl1.OpenGL;
+      
+            gl.Enable(OpenGL.GL_LIGHT0);
+            gl.Enable(OpenGL.GL_LIGHTING);
+            gl.Enable(OpenGL.GL_DEPTH_TEST);
+          
+
+            gl.Material(OpenGL.GL_FRONT, OpenGL.GL_AMBIENT, glfMatAmbient);
+            gl.Material(OpenGL.GL_FRONT, OpenGL.GL_DIFFUSE, glfMatDiffuse);
+            //gl.Material(OpenGL.GL_FRONT, OpenGL.GL_SPECULAR, glfMatSpecular);
+            gl.Material(OpenGL.GL_FRONT, OpenGL.GL_EMISSION, glfMatEmission);
+            gl.Material(OpenGL.GL_FRONT, OpenGL.GL_SHININESS, fShininess);
+
+            obj.loadFile("cube.obj");
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            a = (double)trackBar1.Value/1000 ;
+        }
+    }
+
+}
